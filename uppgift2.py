76de3354c7c4dcf77f38e296a0fef5b7f4999551
Robin Kavenius <robin.kavenius@yh.nackademin.se>
2024-02-27 13:12:51 +0100

    Finished palindromish

--- uppgift2.py
+++ uppgift2.py
@@ -1,6 +1,29 @@
 # Skriv funktionen här!
-# def palindromish <skriv delen med funktionens argument själv
-pass  # Funktionens logik här istället för denna rad
+def palindromish(word, grade):
+    # Convert string to list of chars.
+    if isinstance(word, str):
+        word = list(word)
+
+    # Get half length of the word. Dont care about the
+    # middle character if it is uneven, so using floor division.
+    half_word = len(word) // 2
+
+    # Check so that grade is below half length, else set it.
+    if grade > half_word:
+        grade = half_word
+
+    count = 0
+    # Iterate over first and last char of the list, comparing them.
+    # -abs() gets the negative index of the word to read backwards,
+    # but 0 is not a negative index so we need index + 1
+    while count < half_word and word[count] == word[-abs(count + 1)]:
+        count += 1
+
+    if count == grade:
+        return True
+
+    return False
+
 
 if __name__ == "__main__":
     # Skriv exempel på körningar av funktionen här
@@ -9,4 +32,9 @@ if __name__ == "__main__":
     # print(palindromish("radar", 2))
     # print(palindromish([1, 2, 3, 2, 1], 3))
     # Lägg till fler exempel som visar hur din funktion fungerar
-    pass  # Ta bort denna rad
+    print(palindromish("radar", 2))  # True
+    print(palindromish("tenet", 5))  # True
+    print(palindromish("hallojsan", 10))  # False
+    print(palindromish("Tenet", 3))  # False
+    print(palindromish([1, 2, 3, 50, 60, 3, 2, 1], 3))  # True
+    print(palindromish([1, 2, 3, 50, 60, 3, 2, 1], 4))  # False
